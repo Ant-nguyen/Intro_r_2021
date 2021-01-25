68dd3cc2838ac77af59e8fcfdc6b01110d871066
Ant-nguyen <77247395+Ant-nguyen@users.noreply.github.com>
2021-01-25 18:39:32 -0500

    Update Module 3.R

--- Module 3.R
+++ Module 3.R
@@ -1,4 +1,4 @@
 Name <-c("Jeb","Donald","Ted","Marco","Carly", "Hillary", "Bernie")
 ABC <- c(4, 62, 51, 21, 2, 14, 15)
 CBS <- c(12, 75, 43, 19, 1, 21, 19)
-poll <- data.frame(Name,ABC,CBS)
+polldf <- data.frame(Name,ABC,CBS)
