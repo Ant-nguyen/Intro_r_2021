4716d0ac3b3cc5723dd7c0926f4b8d4151e4709d
Ant-nguyen <77247395+Ant-nguyen@users.noreply.github.com>
2021-02-16 11:28:53 -0500

    Create Module 6.R

---
+++ Module 6.R
@@ -0,0 +1,9 @@
+A <- matrix(c(2,0,1,3),ncol = 2)
+B <- matrix(c(5,2,4,-1),ncol = 2)
+A+B
+A-B
+diag(c(4,1,2,3))
+m <- diag(3,ncol = 5,nrow = 5) #Create a 5x5 with 3 across all the diagnol position
+m[2:5,1] <- 2 # makes all the values on col 1 = 2, except row 1
+m[1,2:5] <- 1 # makes all the values on row 1 = 1, except col 1
+m
