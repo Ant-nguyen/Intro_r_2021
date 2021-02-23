47e8d9768dd106f71c15f12f6345652ad4c1e8c4
Ant-nguyen <77247395+Ant-nguyen@users.noreply.github.com>
2021-02-22 21:08:19 -0500

    Create Module7.R

---
+++ Module7.R
@@ -0,0 +1,31 @@
+fileName <- 'HPV-10.txt' #Text file with the full HPV10 genome
+HPV10 <- readChar(fileName,file.info(fileName)$size)
+print(HPV10)
+summary(HPV10)
+
+#s3 class creation
+capsid <- list(DNAseq= substr(HPV10,443,1375),size = nchar(substr(HPV10,443,1375)))
+class(capsid) <-"coding"
+# s3 Constructor function for our classes
+Coding <- function(DNA){
+  cod <- list(DNAseq = DNA, size = nchar(DNA), Fiveto3 = TRUE)
+  class(cod) <- "coding"
+  return(cod)
+}
+Template <- function(DNA){
+  temp <- list(DNAseq = DNA, size = nchar(DNA), Fiveto3 = FALSE)
+  class(temp) <- "template"
+  return(temp)
+}
+#custom print method for our new classes
+print.coding <- function(item){
+  cat("5\'",item$DNAseq,"3\'")
+}
+
+print.template <- function(item){
+  cat("3\'",item$DNAseq,"5\'")
+}
+# s4 class creation
+setClass("coding",representation(DNAseq = "character",size = "numeric",Fiveto3 ="logical"))
+setClass("template",representation(DNAseq = "character",size = "numeric",Fiveto3 ="logical"))
+capsid <- new("coding",DNAseq= substr(HPV10,443,1375),size = nchar(substr(HPV10,443,1375)),Fiveto3 = TRUE)
