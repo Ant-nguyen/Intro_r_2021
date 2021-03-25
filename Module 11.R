26144593db03d44bfb2f378468ce85c07e4b2470
Ant-nguyen <77247395+Ant-nguyen@users.noreply.github.com>
2021-03-24 21:09:45 -0400

    Update Module 11.R

--- Module 11.R
+++ Module 11.R
@@ -9,7 +9,7 @@ tukey_multiple <- function(x) {
   { outlier.vec[i] <- all(outliers[i,]) } 
   return(outlier.vec) }
 
-tukey.outlier <- function(x) {
+tukey.outlier <- function(x) { #possible tukey.outlier function
   quant <- quantile(x)
   iqr <- quant[4]-quant[2]
   return(any(x <(quant[2]-(1.5*iqr))|any(x > (quant[4]+(1.5*iqr)))))
